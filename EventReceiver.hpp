8984b4219a52cd0ab0e640c665f054f23cdb4896
MissValeska <valeska_grim@yahoo.com>
2014-01-29 11:48:48 -0800

    Added EventReciver.hpp

---
+++ EventReceiver.hpp
@@ -0,0 +1,34 @@
+#ifndef EVENTRECEIVER_HPP_INCLUDED
+#define EVENTRECEIVER_HPP_INCLUDED
+
+class MyEventReceiver : public IEventReceiver
+{
+public:
+    // This is the one method that we have to implement
+    virtual bool OnEvent(const SEvent& event)
+    {
+        // Remember whether each key is down or up
+        if (event.EventType == irr::EET_KEY_INPUT_EVENT)
+            KeyIsDown[event.KeyInput.Key] = event.KeyInput.PressedDown;
+
+        return false;
+    }
+
+    // This is used to check whether a key is being held down
+    virtual bool IsKeyDown(EKEY_CODE keyCode) const
+    {
+        return KeyIsDown[keyCode];
+    }
+
+    MyEventReceiver()
+    {
+        for (u32 i=0; i<KEY_KEY_CODES_COUNT; ++i)
+            KeyIsDown[i] = false;
+    }
+
+private:
+    // We use this array to store the current state of each key
+    bool KeyIsDown[KEY_KEY_CODES_COUNT];
+};
+
+#endif // EVENTRECEIVER_HPP_INCLUDED
