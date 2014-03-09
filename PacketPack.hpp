7b07d490e674b4b17cf04a2ab1fe43d4bc2068da
Michele Bini <michele.bini@gmail.com>
2014-03-09 04:42:09 +0100

    Add PacketPack.hpp library

---
+++ PacketPack.hpp
@@ -0,0 +1,49 @@
+#include <cstdint>
+#include <string>
+#include <sstream>
+ 
+// #include <arpa/inet.h> // might be needed for hton* functions
+ 
+#include <irrlicht/vector3d.h>
+ 
+#include "UDPSocket.hpp"
+ 
+struct PacketPack : public std::basic_stringstream<char> {
+    PacketPack() {};
+ 
+    PacketPack& operator <<(f32 v) {
+        // Convert number to fixed-point
+        int32_t vv = ((double)v)*1000;
+        std::basic_stringstream<char> &s = *this;
+        s << ((uint8_t)((vv)       &0xff));
+        s << ((uint8_t)((vv >> 8)  &0xff));
+        s << ((uint8_t)((vv >> 16) &0xff));
+        s << ((uint8_t)((vv >> 24) &0xff));
+        return (*this);
+    }
+    
+    PacketPack& operator <<(const core::vector3df& v) {
+      (*this) << v.X << v.Y << v.Z;
+      return (*this);
+    }
+};
+
+struct PacketUnpack : public std::basic_istringstream<char> {
+  PacketUnpack(const std::basic_string<char> &s) :std::basic_istringstream<char>(s) {};
+ 
+    PacketUnpack& operator >>(f32 &v) {
+        uint8_t a, b, c, d;
+        std::basic_istringstream<char> &s = *this;
+        s >> a >> b >> c >> d;
+        int32_t vv = a | (b<<8) | (c<<16) | (d<<24);
+        // Convert number from fixed-point
+        v = ((double)vv)/1000;
+        return (*this);
+    }
+    
+    PacketUnpack& operator >>(core::vector3df& v) {
+      (*this) >> v.X >> v.Y >> v.Z;
+      return (*this);
+    }
+    
+};
