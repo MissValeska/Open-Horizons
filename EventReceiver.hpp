54665b6106653f91f996c2c4f7644ac623e644e0
MissValeska <valeska_grim@yahoo.com>
2014-03-30 18:49:47 -0700

    The Beginnings of User Interface stuff.

--- EventReceiver.hpp
+++ EventReceiver.hpp
@@ -7,9 +7,38 @@ using namespace irr;
 
 class EventReceiver : public IEventReceiver
 {
-
-
 	public:
+
+		// Declare a structure to hold some context for the event receiver so that it
+		// has it available inside its OnEvent() method.
+		struct SAppContext
+		{
+		    IrrlichtDevice *device;
+		    s32             counter;
+		    irr::gui::IGUIListBox*    listbox;
+		};
+
+		// Define some values that we'll use to identify individual GUI controls.
+		enum
+		{
+   		 GUI_ID_QUIT_BUTTON = 101,
+   		GUI_ID_NEW_WINDOW_BUTTON,
+   		 GUI_ID_FILE_OPEN_BUTTON,
+    		GUI_ID_TRANSPARENCY_SCROLL_BAR
+		};
+
+		EventReceiver(SAppContext & context) : Context(context) { }
+
+void setSkinTransparency(s32 alpha, irr::gui::IGUISkin * skin)
+		{
+    		for (s32 i=0; i<irr::gui::EGDC_COUNT ; ++i)
+    		{
+        	video::SColor col = skin->getColor((irr::gui::EGUI_DEFAULT_COLOR)i);
+        	col.setAlpha(alpha);
+        	skin->setColor((irr::gui::EGUI_DEFAULT_COLOR)i, col);
+    		}
+	}
+
 		//!< We'll create a struct to record info on the mouse state
 		struct SMouseState
 		{
@@ -45,11 +74,88 @@ class EventReceiver : public IEventReceiver
 				}
 			}
 
+	if (event.EventType == EET_GUI_EVENT)
+        {
+            s32 id = event.GUIEvent.Caller->getID();
+            irr::gui::IGUIEnvironment* env = Context.device->getGUIEnvironment();
+
+            switch(event.GUIEvent.EventType)
+            {
+	case irr::gui::EGET_SCROLL_BAR_CHANGED:
+                if (id == GUI_ID_TRANSPARENCY_SCROLL_BAR)
+                {
+                    s32 pos = ((irr::gui::IGUIScrollBar*)event.GUIEvent.Caller)->getPos();
+                    setSkinTransparency(pos, env->getSkin());
+                }
+                break;
+
+	case irr::gui::EGET_BUTTON_CLICKED:
+                switch(id)
+                {
+                case GUI_ID_QUIT_BUTTON:
+                    Context.device->closeDevice();
+                    return true;
+
+                case GUI_ID_NEW_WINDOW_BUTTON:
+                    {
+                    Context.listbox->addItem(L"Window created");
+                    Context.counter += 30;
+                    if (Context.counter > 200)
+                        Context.counter = 0;
+
+                    irr::gui::IGUIWindow* window = env->addWindow(
+                        irr::core::rect<s32>(100 + Context.counter, 100 + Context.counter, 300 + Context.counter, 200 + Context.counter),
+                        false, // modal?
+                        L"Test window");
+
+                    env->addStaticText(L"Please close me",
+                        irr::core::rect<s32>(35,35,140,50),
+                        true, // border?
+                        false, // wordwrap?
+                        window);
+                    }
+                    return true;
+
+                case GUI_ID_FILE_OPEN_BUTTON:
+                    Context.listbox->addItem(L"File open");
+                    // There are some options for the file open dialog
+                    // We set the title, make it a modal window, and make sure
+                    // that the working directory is restored after the dialog
+                    // is finished.
+                    env->addFileOpenDialog(L"Please choose a file.", true, 0, -1, true);
+                    return true;
+
+                default:
+                    return false;
+                }
+                break;
+
+            case irr::gui::EGET_FILE_SELECTED:
+                {
+                    // show the model filename, selected in the file dialog
+                    irr::gui::IGUIFileOpenDialog* dialog =
+                        (irr::gui::IGUIFileOpenDialog*)event.GUIEvent.Caller;
+                    Context.listbox->addItem(dialog->getFileName());
+                }
+                break;
+
+            default:
+                break;
+            }
+        }
+
+        return false;
+
+private:
+    SAppContext & Context;
+
+public:
+
 			//!< The state of each connected joystick is sent to us
 			//!< once every run() of the Irrlicht device.  Store the
 			//!< state of the first joystick, ignoring other joysticks.
 			//!< This is currently only supported on Windows and Linux.
-			if (event.EventType == irr::EET_JOYSTICK_INPUT_EVENT
+			if(event.EventType == irr::EET_JOYSTICK_INPUT_EVENT
 					&& event.JoystickEvent.Joystick == 0)
 			{
 				JoystickState = event.JoystickEvent;
@@ -58,11 +164,9 @@ class EventReceiver : public IEventReceiver
 			enum keyStatesENUM {UP, DOWN, PRESSED, RELEASED};
 
 			//!< Keyboard key states. Unused?
-			/*
 			   keyStatesENUM keyState[KEY_KEY_CODES_COUNT];
 
 			   EKEY_CODE keyCode;
-			 */
 
 			//!< Remember whether each key is down or up
 			if (event.EventType == irr::EET_KEY_INPUT_EVENT)
--- main.cpp
+++ main.cpp
@@ -20,8 +20,7 @@ using namespace gui;
 
 int main(int argc, char ** argv) //!<!<  The options here define an argument count apparently, I don't fully understand it.
 	//!<!< http://!<collabedit.com/sab53 A friend explained here if it is still up next you check.
-{
-
+{	
 	options_select();
 
 	EventReceiver receiver;
@@ -45,6 +44,52 @@ int main(int argc, char ** argv) //!<!<  The options here define an argument cou
 	//!<!< Get the Scene Manager from the device.
 	ISceneManager * smgr = device->getSceneManager();
 	if (!smgr) fatal("Fatal Error: Could not get Scene Manager from the Irrlicht Device.", 4);
+
+    device->setWindowCaption(L"Irrlicht Engine - User Interface Demo");
+    device->setResizable(true);
+
+    video::IVideoDriver* driver = device->getVideoDriver();
+    IGUIEnvironment* env = device->getGUIEnvironment();
+
+    IGUISkin* skin = env->getSkin();
+    IGUIFont* font = env->getFont("Fonts/fonthaettenschweiler.bmp");
+    if (font)
+        skin->setFont(font);
+
+    skin->setFont(env->getBuiltInFont(), EGDF_TOOLTIP);
+
+    env->addButton(rect<s32>(10,240,110,240 + 32), 0, GUI_ID_QUIT_BUTTON,
+            L"Quit", L"Exits Program");
+    env->addButton(rect<s32>(10,280,110,280 + 32), 0, GUI_ID_NEW_WINDOW_BUTTON,
+            L"New Window", L"Launches a new Window");
+    env->addButton(rect<s32>(10,320,110,320 + 32), 0, GUI_ID_FILE_OPEN_BUTTON,
+            L"File Open", L"Opens a file");
+	
+	    env->addStaticText(L"Transparent Control:", rect<s32>(150,20,350,40), true);
+    IGUIScrollBar* scrollbar = env->addScrollBar(true,
+            rect<s32>(150, 45, 350, 60), 0, GUI_ID_TRANSPARENCY_SCROLL_BAR);
+    scrollbar->setMax(255);
+    scrollbar->setPos(255);
+    setSkinTransparency( scrollbar->getPos(), env->getSkin());
+
+    // set scrollbar position to alpha value of an arbitrary element
+    scrollbar->setPos(env->getSkin()->getColor(EGDC_WINDOW).getAlpha());
+
+    env->addStaticText(L"Logging ListBox:", rect<s32>(50,110,250,130), true);
+    IGUIListBox * listbox = env->addListBox(rect<s32>(50, 140, 250, 210));
+    env->addEditBox(L"Editable Text", rect<s32>(350, 80, 550, 100));
+
+    // Store the appropriate data in a context structure.
+    SAppContext context;
+    context.device = device;
+    context.counter = 0;
+    context.listbox = listbox;
+
+    // Then create the event receiver, giving it that context structure.
+    EventReceiver receiver(context);
+
+    // And tell the device to use our custom event receiver.
+    device->setEventReceiver(&receiver);
 
 	//!<!< Add a Cube to the Scene.
 	ISceneNode * n = smgr->addCubeSceneNode();
